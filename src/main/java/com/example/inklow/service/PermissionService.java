98d39d4610d9ec1dd39037ed117a1f4a541132b0
BlankSpot08 <arvinchu31@gmail.com>
2021-01-04 19:47:34 +0800

    Commit: 1.9

---
+++ src/main/java/com/example/inklow/service/PermissionService.java
@@ -0,0 +1,18 @@
+package com.example.inklow.service;
+
+import com.example.inklow.entities.Permission;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public interface PermissionService {
+    List<Permission> getListOfPermission();
+
+    Permission getPermissionById(UUID id);
+    Permission getPermissionByName(String name);
+
+    Permission handlePermissionRegistration(Permission permission);
+    Permission handlePermissionDeletion(Permission permission);
+}
